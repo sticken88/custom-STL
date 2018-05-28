fc51163affaec792f4879624bc5a378b64040cd9
Matteo Sticco <matteo.sticco@gmail.com>
2018-05-28 21:38:10 +0200

    Implemented 'push_front', 'pop_front' and some helper methods.

--- forward_list/forward_list.h
+++ forward_list/forward_list.h
@@ -1,6 +1,8 @@
 #ifndef __FORWARD_LIST__
 #define __FORWARD_LIST__
 
+#include <iostream>
+
 namespace mst
 {
 template <class T>
@@ -9,9 +11,41 @@ template <class T>
    public:
       ForwardList(){
          head = NULL;
+         size = 0;
       }
 
       ~ForwardList(){}
+
+      void push_front(T value)
+      {
+         // fist create the new node
+         list_node* new_node = create_node(value);
+         // add it to the list
+         new_node->next = head;
+         head = new_node;
+         size++;
+      }
+
+      void pop_front(void)
+      {
+         // copy the header
+         list_node* _tmp = head;
+         // update the head
+         head = head->next;
+         // delete the old head
+         delete _tmp;
+         size--;
+      }
+
+      void print(void)
+      {
+         while(head != NULL)
+         {
+            std::cout << head->value << std::endl;
+            head = head->next;
+         }
+      }
+
    private:
       /* Define the stucture of each node */
       struct list_node{
@@ -20,6 +54,17 @@ template <class T>
       };
 
       list_node *head;
+      int size;
+
+      /* Used to create a new node */
+      list_node* create_node(T& value)
+      {
+         list_node* node = new list_node;
+         node->value = value;
+         node->next = NULL;
+
+         return node;
+      }
    };
 };
 
